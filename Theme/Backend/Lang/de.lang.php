06f6d25744517907d29f6025c1933ecc78892f9a
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-04-05 19:31:55 +0200

    add/improve locale

---
+++ Theme/Backend/Lang/de.lang.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package   Modules\Calendar
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['Calendar' => [
+    'Blocks'    => 'Blöcke',
+    'Calendar'  => 'Kalender',
+    'Close'     => 'Schließen',
+    'Day'       => 'Tag',
+    'Event'     => 'Event',
+    'Friday'    => 'Freitag',
+    'Interval'  => 'Intervall',
+    'Layout'    => 'Layout',
+    'List'      => 'Liste',
+    'Monday'    => 'Montag',
+    'Month'     => 'Monat',
+    'NewEvent'  => 'Neues Event',
+    'Saturday'  => 'Samstag',
+    'Settings'  => 'Einstellung',
+    'Sunday'    => 'Sonntag',
+    'Thursday'  => 'Donnerstag',
+    'Timeline'  => 'Zeitlinie',
+    'Tuesday'   => 'Dienstag',
+    'Wednesday' => 'Mittwoch',
+    'Week'      => 'Woche',
+    'Year'      => 'Jahr',
+]];
