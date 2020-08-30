cbdd8635e77978b56598b917b96d03a6f825fa5b
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-08-30 20:13:10 +0200

    autofixes

--- Models/ScheduleStatus.php
+++ Models/ScheduleStatus.php
@@ -27,5 +27,6 @@ use phpOMS\Stdlib\Base\Enum;
 abstract class ScheduleStatus extends Enum
 {
     public const ACTIVE   = 1;
+
     public const INACTIVE = 1;
 }
--- tests/Admin/AdminTest.php
+++ tests/Admin/AdminTest.php
@@ -20,6 +20,7 @@ namespace Modules\Calendar\tests\Admin;
 class AdminTest extends \PHPUnit\Framework\TestCase
 {
     protected const MODULE_NAME = 'Calendar';
+
     protected const URI_LOAD = 'http://127.0.0.1/en/backend/calendar';
 
     use \Modules\tests\ModuleTestTrait;
