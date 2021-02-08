199b6e5165dd0eb657140eb913df78e7cd4348cd
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-02-08 19:10:15 +0100

    test language/export/import which changed a lot of lang files

--- Theme/Backend/Lang/de.lang.php
+++ Theme/Backend/Lang/de.lang.php
@@ -4,7 +4,6 @@
  *
  * PHP Version 8.0
  *
- * @package   Modules\Calendar
  * @copyright Dennis Eichhorn
  * @license   OMS License 1.0
  * @version   1.0.0
@@ -13,25 +12,25 @@
 declare(strict_types=1);
 
 return ['Calendar' => [
-    'Blocks'    => 'Blöcke',
-    'Calendar'  => 'Kalender',
-    'Close'     => 'Schließen',
-    'Day'       => 'Tag',
-    'Event'     => 'Event',
-    'Friday'    => 'Freitag',
-    'Interval'  => 'Intervall',
-    'Layout'    => 'Layout',
-    'List'      => 'Liste',
-    'Monday'    => 'Montag',
-    'Month'     => 'Monat',
-    'NewEvent'  => 'Neues Event',
-    'Saturday'  => 'Samstag',
-    'Settings'  => 'Einstellung',
-    'Sunday'    => 'Sonntag',
-    'Thursday'  => 'Donnerstag',
-    'Timeline'  => 'Zeitlinie',
-    'Tuesday'   => 'Dienstag',
+    'Blocks' => 'Blöcke',
+    'Calendar' => 'Kalender',
+    'Close' => 'Schließen',
+    'Day' => 'Tag',
+    'Event' => 'Event',
+    'Friday' => 'Freitag',
+    'Interval' => 'Intervall',
+    'Layout' => 'Layout',
+    'List' => 'Liste',
+    'Monday' => 'Montag',
+    'Month' => 'Monat',
+    'NewEvent' => 'Neues Event',
+    'Saturday' => 'Samstag',
+    'Settings' => 'Einstellung',
+    'Sunday' => 'Sonntag',
+    'Thursday' => 'Donnerstag',
+    'Timeline' => 'Zeitlinie',
+    'Tuesday' => 'Dienstag',
     'Wednesday' => 'Mittwoch',
-    'Week'      => 'Woche',
-    'Year'      => 'Jahr',
+    'Week' => 'Woche',
+    'Year' => 'Jahr',
 ]];
--- Theme/Backend/Lang/en.lang.php
+++ Theme/Backend/Lang/en.lang.php
@@ -4,7 +4,6 @@
  *
  * PHP Version 8.0
  *
- * @package   Modules\Calendar
  * @copyright Dennis Eichhorn
  * @license   OMS License 1.0
  * @version   1.0.0
@@ -13,25 +12,25 @@
 declare(strict_types=1);
 
 return ['Calendar' => [
-    'Blocks'    => 'Blocks',
-    'Calendar'  => 'Calendar',
-    'Close'     => 'Close',
-    'Day'       => 'Day',
-    'Event'     => 'Event',
-    'Friday'    => 'Friday',
-    'Interval'  => 'Interval',
-    'Layout'    => 'Layout',
-    'List'      => 'List',
-    'Monday'    => 'Monday',
-    'Month'     => 'Month',
-    'NewEvent'  => 'New Event',
-    'Saturday'  => 'Saturday',
-    'Settings'  => 'Settings',
-    'Sunday'    => 'Sunday',
-    'Thursday'  => 'Thursday',
-    'Timeline'  => 'Timeline',
-    'Tuesday'   => 'Tuesday',
+    'Blocks' => 'Blocks',
+    'Calendar' => 'Calendar',
+    'Close' => 'Close',
+    'Day' => 'Day',
+    'Event' => 'Event',
+    'Friday' => 'Friday',
+    'Interval' => 'Interval',
+    'Layout' => 'Layout',
+    'List' => 'List',
+    'Monday' => 'Monday',
+    'Month' => 'Month',
+    'NewEvent' => 'New Event',
+    'Saturday' => 'Saturday',
+    'Settings' => 'Settings',
+    'Sunday' => 'Sunday',
+    'Thursday' => 'Thursday',
+    'Timeline' => 'Timeline',
+    'Tuesday' => 'Tuesday',
     'Wednesday' => 'Wednesday',
-    'Week'      => 'Week',
-    'Year'      => 'Year',
+    'Week' => 'Week',
+    'Year' => 'Year',
 ]];
