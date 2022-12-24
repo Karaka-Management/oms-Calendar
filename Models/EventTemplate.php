94d2e76016785f27182af7e1335db557157c42a2
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-12-25 00:06:17 +0100

    test fixes and changes for release

--- Models/EventTemplate.php
+++ Models/EventTemplate.php
@@ -31,4 +31,30 @@ class EventTemplate extends Event
      * @since 1.0.0
      */
     private int $type = EventType::TEMPLATE;
+
+    /**
+     * Get event type.
+     *
+     * @return int
+     *
+     * @since 1.0.0
+     */
+    public function getType() : int
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set event type.
+     *
+     * @param int $type Event type
+     *
+     * @return void
+     *
+     * @since 1.0.0
+     */
+    public function setType(int $type = EventType::TEMPLATE) : void
+    {
+        $this->type = $type;
+    }
 }
