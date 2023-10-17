2f3dc935ccec2a71fc9bc97474bcb68f6cf7e9ce
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-17 00:13:58 +0000

    fix jsonSerialize function call

--- tests/Models/NullCalendarTest.php
+++ tests/Models/NullCalendarTest.php
@@ -47,6 +47,6 @@ final class NullCalendarTest extends \PHPUnit\Framework\TestCase
     public function testJsonSerialize() : void
     {
         $null = new NullCalendar(2);
-        self::assertEquals(['id' => 2], $null);
+        self::assertEquals(['id' => 2], $null->jsonSerialize());
     }
 }
--- tests/Models/NullEventTest.php
+++ tests/Models/NullEventTest.php
@@ -47,6 +47,6 @@ final class NullEventTest extends \PHPUnit\Framework\TestCase
     public function testJsonSerialize() : void
     {
         $null = new NullEvent(2);
-        self::assertEquals(['id' => 2], $null);
+        self::assertEquals(['id' => 2], $null->jsonSerialize());
     }
 }
