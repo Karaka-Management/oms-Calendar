8a0fd81f97bb45be7a10ff99ccf17f80119c6470
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-01-02 23:34:17 +0000

    update

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -34,7 +34,7 @@ use phpOMS\Views\View;
 final class BackendController extends Controller implements DashboardElementInterface
 {
     /**
-     * Routing end-point for application behaviour.
+     * Routing end-point for application behavior.
      *
      * @param RequestAbstract  $request  Request
      * @param ResponseAbstract $response Response
--- Models/PermissionCategory.php
+++ Models/PermissionCategory.php
@@ -17,7 +17,7 @@ namespace Modules\Calendar\Models;
 use phpOMS\Stdlib\Base\Enum;
 
 /**
- * Permision state enum.
+ * Permission category enum.
  *
  * @package Modules\Calendar\Models
  * @license OMS License 2.0
