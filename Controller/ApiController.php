52e488f257e9fd13dfe1266afe75a1b42fd8bfd3
Dennis Eichhorn <spl1nes.com@googlemail.com>
2024-05-21 00:09:07 +0200

    fix version and bugs

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\Calendar
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -18,7 +18,7 @@ namespace Modules\Calendar\Controller;
  * Calendar api controller class.
  *
  * @package Modules\Calendar
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  */
